da9ce943699e84940b39c002cb487f4a638cd562
iandillon <ian.dillon@freshwater-uk.com>
2016-06-15 20:16:53 +0100

    Extend Leblender Grid Converter to render with typed view
    Changes made ensure converter only runs for leblender grid controls,
    Added config converter and means of re-routing render to a view suitable
    for typed models

---
+++ Config/GridEditorLeBlenderConfig.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Skybrud.Umbraco.GridData;
+using Skybrud.Umbraco.GridData.Extensions.Json;
+using Skybrud.Umbraco.GridData.Interfaces;
+using Skybrud.Umbraco.GridData.Json;
+
+namespace Opten.Umbraco.GridData.Config
+{
+    public class GridEditorLeBlenderConfig : GridJsonObject, IGridEditorConfig
+	{
+		#region Properties
+
+
+        [JsonProperty("renderInGrid", NullValueHandling = NullValueHandling.Ignore)]
+        public string RenderInGrid { get; private set; }
+
+
+		[JsonProperty("frontView", NullValueHandling = NullValueHandling.Ignore)]
+        public string FrontView { get; private set; }
+
+        public GridEditor Editor { get; private set; }
+
+        public string Guid { get; private set; }
+
+
+		#endregion
+
+		#region Constructors
+
+        private GridEditorLeBlenderConfig(GridEditor editor, JObject obj)
+            : base(obj)
+        {
+            Editor = editor;
+            JToken guidJToken;
+            if ((guidJToken = editor.Control.JObject.GetValue("guid")) != null)
+            {
+                Guid = guidJToken.Value<string>();
+            }
+            
+        }
+
+		#endregion
+
+		#region Static methods
+
+
+        public static GridEditorLeBlenderConfig Parse(GridEditor editor, JObject obj)
+		{
+            if (obj == null) return null;
+            return new GridEditorLeBlenderConfig(editor, obj)
+            {
+                FrontView = obj.GetString("frontView"),
+                RenderInGrid = obj.GetString("renderInGrid")
+            };
+		}
+		
+		#endregion
+	}
+}
